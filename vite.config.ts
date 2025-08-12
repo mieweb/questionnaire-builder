6449b17cb5520ecde33761b3c4853cec819c2308
Lin Latt <lattln.tech@gmail.com>
2025-08-12 14:55:04 -0400

    update workflow
    update workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 
     server: {
       host: '0.0.0.0',
+      port: 5173,
       allowedHosts: ['lattln-questionnaire-builder-main.opensource.mieweb.org']
     },
     plugins: [
