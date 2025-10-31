0c3fddf16084d4940ecba495f2dda53de8f56e00
Lin Latt <llatt@mieweb.com>
2025-10-31 16:42:52 -0400

    update workflow
    + update launchpad port
    + update ports

--- apps/web-demo-packages/vite.config.ts
+++ apps/web-demo-packages/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: 5180,
+    port: 3000,
     strictPort: false,
   },
   optimizeDeps: {
